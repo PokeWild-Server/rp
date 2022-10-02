0ca610473aeeaff4d57d377f08224acecc4451f4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-01 20:37:08 -0500

    update

--- clean.sh
+++ clean.sh
@@ -1,1 +1,7 @@
-./PackSquash-*.AppImage settings.toml
+#./PackSquash-*.AppImage settings.toml
+cp my_pack.zip lite.zip
+zip -d lite.zip "assets/minecraft/blockstates/*"
+zip -d lite.zip "assets/minecraft/models/block/*"
+zip -d lite.zip "assets/minecraft/textures/block/*"
+zip -d lite.zip "assets/minecraft/textures/entity/signs/*"
+zip -d lite.zip "assets/minecraft/textures/environment/*"
